ca416eb003f9283e8d87e8498a3ee6c2557ffefe
anitahx <anitaizzy4@gmail.com>
2022-08-25 05:25:34 -0700

    bug fixes

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
+    n = len(sys.argv)
+    sum = 0
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+        print(sum)
