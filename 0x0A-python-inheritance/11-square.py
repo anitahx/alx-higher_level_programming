5ed450cd6cd5f5e7a129455abf50a312512d6dcc
anitahx <anitaizzy4@gmail.com>
2022-09-26 07:55:33 -0700

    last commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+"""
+more class base
+"""
 
-s = Square(13)
 
-print(s)
-print(s.area())
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""
+Square class
+"""
+
+
+class Square(Rectangle):
+    """ Square Class """
+    def __init__(self, size):
+        """ size init"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
