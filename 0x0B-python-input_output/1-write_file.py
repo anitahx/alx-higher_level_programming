320602bf06ffa3fc94c1ba438a881fdc55657fe3
anitahx <anitaizzy4@gmail.com>
2022-09-27 04:15:57 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-''' function that returns the number of lines of a text file
-'''
+"""Module to write to a file"""
 
 
-def number_of_lines(filename=""):
-    ''' function: number_of_lines
-    '''
-    if filename == "" or type(filename) != str:
-        return 0
-    counter = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            counter += 1
-    return counter
+def write_file(filename="", text=""):
+    '''Writes a string to text file and returns string length'''
+    with open(filename, 'w', encoding="utf-8") as f:
+        for char in text:
+            f.write(char)
+    return len(text)
